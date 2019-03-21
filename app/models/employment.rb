a80a94fa44b688ad599d84c9459871dfd4a4863e
Benard Ochang <realtorben2012@gmail.com>
2019-03-21 07:18:16 +0000

    employees dropped

--- app/models/employment.rb
+++ app/models/employment.rb
@@ -1,4 +1,3 @@
 class Employment < ApplicationRecord
-    belongs_to :worker
-    belongs_to :company
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190310132529) do
+ActiveRecord::Schema.define(version: 20190321071435) do
 
   create_table "companies", force: :cascade do |t|
     t.string   "name"
@@ -29,16 +29,6 @@ ActiveRecord::Schema.define(version: 20190310132529) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["company_id"], name: "index_departments_on_company_id"
-  end
-
-  create_table "employees", force: :cascade do |t|
-    t.integer  "company_id"
-    t.string   "firstname"
-    t.string   "code"
-    t.string   "nationalid"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["company_id"], name: "index_employees_on_company_id"
   end
 
   create_table "employments", force: :cascade do |t|
