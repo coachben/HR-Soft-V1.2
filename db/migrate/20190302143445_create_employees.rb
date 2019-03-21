7a594f5909b714949a6a53b8ceeb3d75ec4f9e7a
Benard Ochang <realtorben2012@gmail.com>
2019-03-21 07:35:59 +0000

    employee ifx

--- db/migrate/20190302143445_create_employees.rb
+++ db/migrate/20190302143445_create_employees.rb
@@ -1,7 +1,7 @@
 class CreateEmployees < ActiveRecord::Migration[5.0]
   def change
     create_table :employees do |t|
-      t.belongs_to :company, index: true, foreign_key: true
+      #t.belongs_to :company, index: true, foreign_key: true
       t.string :firstname
       t.string :firstname
       t.string :code
